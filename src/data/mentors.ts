962d6180fdade9a9c753ac2bed7b1cdb057ddf65
bi0sHardware <112330152+bi0sHardware@users.noreply.github.com>
2022-11-27 20:15:55 +0530

    Update mentors.ts
    season bhaiya update

--- src/data/mentors.ts
+++ src/data/mentors.ts
@@ -1,40 +1,13 @@
 const mentors = [
-    // {
-    //     campus: "Amritapuri",
-    //     firstname: 'Abhinand',
-    //     lastname: 'N',
-    //     category: 'Android Security',
-    //     team: 'CTF'
-    // },
-    // {
-    //     campus: "Amritapuri",
-    //     firstname: 'Arjun',
-    //     lastname: 'TU',
-    //     category: 'Reverse Engineering',
-    //     team: 'CTF'
-    // },
-    // {
-    //     campus: "Amritapuri",
-    //     firstname: 'Aswin',
-    //     lastname: 'M Guptha',
-    //     category: 'Web Security',
-    //     team: 'CTF'
-    // },
     {
         campus: "Amritapuri",
         firstname: 'Season',
         lastname: 'Cherian',
-        category: 'Hardware Security',
+        category: 'OT Security',
         team: 'Hardware',
         img:'/images/team/season.png'
     },
-    // {
-    //     campus: "Amritapuri",
-    //     firstname: 'Varun',
-    //     lastname: 'Nair',
-    //     category: 'Offensive Security',
-    //     team: 'Pentest'
-    // },
+   
     {
         campus: "Amritapuri",
         firstname: 'Vivek',
